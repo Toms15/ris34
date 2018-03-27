9e22719778c889243faabb9dbb24966e864a2457
Tommaso Costantini <info@tommasocostantini.it>
2018-03-27 13:12:02 +0200

    first commit

---
+++ app/assets/js/app.js
@@ -0,0 +1,117 @@
+$(document).foundation();
+$(document).ready(function() {
+
+  // ----------------------------------------
+  // Cookie Bar
+  // ----------------------------------------
+  $.cookieBar({
+   message: "Cliccando su OK, l’utente accetta l’utilizzo dei cookie di terze parti. Per maggiori informazioni, anche in ordine alla disattivazione, è possibile consultare l'<a class='cookie-link-2' href='/cookie.html'>informativa cookie completa</a>.",
+   acceptText: 'Ok',
+   fixed: true,
+   bottom: true
+  });
+
+  // ----------------------------------------
+  // Magnific Popup Gallery
+  // ----------------------------------------
+  $('.container__gallery').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+      enabled:true,
+
+      preload: [1,3],
+
+      navigateByImgClick: true,
+
+      arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+
+      tPrev: 'Previous (Left arrow key)',
+      tNext: 'Next (Right arrow key)',
+      tCounter: '<span class="mfp-counter">%curr% of %total%</span>'
+    }
+  });
+
+	// ----------------------------------------
+  // Sticky Header
+  // ----------------------------------------
+  $(".header__site").sticky({topSpacing:0});
+
+ 	// ----------------------------------------
+  // Toggle Header Menu Mobile
+  // ----------------------------------------
+  $('.mobile__menu').on('click', function() {
+  	$('.entry__menu').toggleClass('show');
+  	$('.mobile__menu').toggleClass('shrink');
+  });
+
+  // ----------------------------------------
+  // Pop up share
+  // ----------------------------------------
+  $('.pop').click(function(event){
+    event.preventDefault();
+    // console.log('clicked');
+
+    newwindow=window.open(jQuery(this).attr('href'),'name','height=450,width=550');
+    if (window.focus) {newwindow.focus()}
+      return false;
+  });
+
+  // ----------------------------------------
+  // Google Maps
+  // ----------------------------------------
+  $(function() {
+    function initialize() {       
+      var mapOptions = {
+        center: new google.maps.LatLng(41.906300,12.480809),
+        zoom: 12,
+        scrollwheel: false,
+        styles: [
+          {
+              "featureType": "administrative.country",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "visibility": "simplified"
+                  },
+                  {
+                      "hue": "#ff0000"
+                  }
+              ]
+          }
+      ]    
+      };
+      var map = new google.maps.Map(document.getElementById("map-ris34"),
+        mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(41.906300,12.480809),
+        map: map
+      });
+    }
+    // google.maps.event.addDomListener(window, 'load', initialize);
+
+    if($('#cookie-bar').length) {
+      $('.cb-enable').on('click', initialize);
+    } else {
+      initialize();
+    }
+
+  });
+
+  $(".language a").click(function(event){
+
+    event.preventDefault();
+
+    var newLang = $(this).data("lang");
+
+    var urlPieces[] = window.location.pathname.split( '/' );
+
+    console.log(urlPieces[1]);
+
+  });
+
+});
+
+
+
---
+++ build/cfgen-form-5/inc/form-config.php
@@ -0,0 +1,72 @@
+<?php
+$cfg['debug'] = false;
+
+// This is the email address where you will receive the notification message
+$cfg['email_address'] = 'ristorante34@yahoo.it';
+
+$cfg['email_from'] = '';
+
+// The recipients in CC and BCC will receive a copy of the data collected in the form
+// Use commas to separate mutiple e-mail addresses (no spaces allowed)
+// Example: youraddress1@yourdomain.com,youraddress2@yourdomain.com
+$cfg['email_address_cc'] = '';
+
+$cfg['email_address_bcc'] = '';
+
+$cfg['emailsendingmethod'] = 'php';
+
+$cfg['smtp_host'] = '';
+
+$cfg['smtp_port'] = '';
+
+$cfg['smtp_encryption'] = '';
+
+$cfg['smtp_username'] = '';
+
+$cfg['smtp_password'] = '';
+
+$cfg['timezone'] = 'UTC';
+
+$cfg['form_id'] = '';
+
+$cfg['form_name'] = 'Ristorante 34 - Ita';
+
+$cfg['form_validationmessage'] = 'Grazie! Il messaggio è stato inviato correttamente.<br />Ti risponderemo il prima possibile.';
+
+$cfg['form_errormessage_captcha'] = 'Captcha non corretto';
+
+$cfg['form_errormessage_emptyfield'] = 'Compila il campo richiesto';
+
+$cfg['form_errormessage_invalidemailaddress'] = 'Indirizzo e-mail non valido';
+
+$cfg['form_errormessage_invalidurl'] = '';
+
+$cfg['form_errormessage_terms'] = '';
+
+$cfg['form_redirecturl'] = '';
+
+$cfg['adminnotification_subject'] = 'Nuovo messaggio dal form del sito Ristorante al 34';
+
+$cfg['adminnotification_hideemptyvalues'] = false;
+
+$cfg['adminnotification_hideformurl'] = true;
+
+$cfg['usernotification_inputid'] = 'cfgen-element-5-3';
+
+$cfg['usernotification_activate'] = false;
+
+$cfg['usernotification_insertformdata'] = false;
+
+$cfg['usernotification_format'] = '';
+
+$cfg['usernotification_subject'] = '';
+
+$cfg['usernotification_message'] = '';
+
+$cfg['usernotification_hideemptyvalues'] = false;
+
+$cfg['formvalidation_required'] = array('cfgen-element-5-6','cfgen-element-5-3');
+
+$cfg['formvalidation_email'] = array('cfgen-element-5-3');
+
+?>
